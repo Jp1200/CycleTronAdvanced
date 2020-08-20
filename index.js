f484990ad3f3a72a5979fe54a5d7b9ce40971544
Jp1200 <jprkr96@gmail.com>
2020-08-20 17:16:21 -0500

    disconnection works

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ console.log("Server has started");
 
 io.on("connection", function (socket) {
   console.log("Connection made");
+
+  socket.on("disconnect", function () {
+    console.log("A player has disconnected");
+  });
 });
